f646a2931c73a94f0642dd4b565bd9cfeb2c8a7a
Sergey Karagodin <patridon87@yandex.ru>
2022-05-25 19:39:18 +0300

    admin handlers and FSM

--- bot_telegram.py
+++ bot_telegram.py
@@ -7,5 +7,6 @@ async def on_startup(_):
     print('Бот онлайн')
 
 client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
--- handlers/admin.py
+++ handlers/admin.py
@@ -1,5 +1,6 @@
-from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher import FSMContext, Dispatcher
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
 from aiogram import types
 from create_bot import dp
 from dotenv import load_dotenv
@@ -19,10 +20,9 @@ class FSMAdmin(StatesGroup):
     answer_2 = State()
     answer_3 = State()
     answer_4 = State()
-    correct_answer_nubmer = State()
+    correct_answer = State()
 
 
-@dp.message_handler(commands='Вопрос', state=None)
 async def new_question(message: types.Message):
     if message.from_user.id == ADMIN:
         await FSMAdmin.photo.set()
@@ -32,10 +32,7 @@ async def new_question(message: types.Message):
         await message.reply('Добавлять вопросы может только администратор')
 
 
-@dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
 async def load_photo(message: types.Message, state: FSMContext):
-    print(message.photo)
-    print(message.text)
     if message.photo:
         async with state.proxy() as data:
             data['photo'] = message.photo[-1].file_id
@@ -43,4 +40,86 @@ async def load_photo(message: types.Message, state: FSMContext):
         async with state.proxy() as data:
             data['photo'] = None
     await FSMAdmin.next()
-    await message.reply('ку')
+    await message.reply('Выбери тему вопроса')
+
+
+async def enter_theme(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['theme'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Напиши текст вопроса')
+
+
+async def enter_text(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['text'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Напиши вариант ответа №1')
+
+
+async def enter_answer_1(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['answer_1'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Напиши вариант ответа №2')
+
+
+async def enter_answer_2(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['answer_2'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Напиши вариант ответа №3')
+
+
+async def enter_answer_3(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['answer_3'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Напиши вариант ответа №4')
+
+
+async def enter_answer_4(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['answer_4'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Какой вариант ответа правильный?')
+
+
+async def enter_correct_answer(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['correct_answer'] = message.text
+        print(data)
+    await state.finish()
+    await message.reply('Вопрос добавлен!')
+
+
+async def cansel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Ввод вопроса отменен')
+
+
+def register_handlers_admin(dp: Dispatcher):
+    dp.register_message_handler(new_question, commands='Вопрос', state=None)
+    dp.register_message_handler(load_photo, content_types=['photo', 'text'],
+                                state=FSMAdmin.photo)
+    dp.register_message_handler(enter_theme, content_types=['text'],
+                                state=FSMAdmin.theme)
+    dp.register_message_handler(enter_text, content_types=['text'],
+                                state=FSMAdmin.text)
+    dp.register_message_handler(enter_answer_1, content_types=['text'],
+                                state=FSMAdmin.answer_1)
+    dp.register_message_handler(enter_answer_2, content_types=['text'],
+                                state=FSMAdmin.answer_2)
+    dp.register_message_handler(enter_answer_3, content_types=['text'],
+                                state=FSMAdmin.answer_3)
+    dp.register_message_handler(enter_answer_4, content_types=['text'],
+                                state=FSMAdmin.answer_4)
+    dp.register_message_handler(enter_correct_answer, content_types=['text'],
+                                state=FSMAdmin.correct_answer)
+    dp.register_message_handler(cansel_handler, state='*', commands='отмена')
+    dp.register_message_handler(cansel_handler,
+                                Text(equals='отмена', ignore_case=True),
+                                state='*')
